441577c9d19a1985a50eb37e6dcd5aa62c1d2091
Rune Rasmussen <rune@ords.io>
2021-10-25 20:07:40 +1300

    animate loading spinner

---
+++ src/lib/component/LoginPage/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+
+import { keyframes } from "@emotion/react";
+
+const bounce = keyframes`
+  from, 20%, 53%, 80%, to {
+    transform: translate3d(0,0,0);
+  }
+
+  40%, 43% {
+    transform: translate3d(0, -30px, 0);
+  }
+
+  70% {
+    transform: translate3d(0, -15px, 0);
+  }
+
+  90% {
+    transform: translate3d(0,-4px,0);
+  }
+`;
+
+export default function LoadingPage() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+        animation: `${bounce} 1s ease infinite`,
+      }}
+    >
+      <CircularProgress />
+    </Box>
+  );
+}
