59ba59669c8f757e81f3494e4de07e3e3f194427
Rune Rasmussen <rune@ords.io>
2021-10-25 21:20:53 +1300

    animation done

--- src/lib/component/LoginPage/index.tsx
+++ src/lib/component/LoginPage/index.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
 
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { keyframes } from "@emotion/react";
+
+import "./styles.css";
 
 const bounce = keyframes`
   from, 20%, 53%, 80%, to {
@@ -22,7 +25,62 @@ const bounce = keyframes`
   }
 `;
 
-export default function LoadingPage() {
+export default function PageTransitionExamples() {
+  const [firstPage, toggleFirstPage] = useState(true);
+
+  useEffect(function () {
+    const interval = setInterval(() => {
+      toggleFirstPage(!firstPage);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  });
+
+  return (
+    <TransitionGroup>
+      {firstPage && (
+        <CSSTransition classNames="item" timeout={500}>
+          <FirstPage />
+        </CSSTransition>
+      )}
+      {!firstPage && (
+        <CSSTransition classNames="item" timeout={500}>
+          <LoadingPage />
+        </CSSTransition>
+      )}
+    </TransitionGroup>
+  );
+}
+
+function Page({ children }: any) {
+  <Box
+    sx={{
+      display: "flex",
+      height: "100vh",
+    }}
+  >
+    {children}
+  </Box>;
+}
+
+function FirstPage() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        height: "100vh",
+        background: "black",
+        color: "white",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      HEEELO
+    </Box>
+  );
+}
+
+function LoadingPage() {
   return (
     <Box
       sx={{
@@ -30,10 +88,11 @@ export default function LoadingPage() {
         height: "100vh",
         justifyContent: "center",
         alignItems: "center",
-        animation: `${bounce} 1s ease infinite`,
       }}
     >
-      <CircularProgress />
+      <Box sx={{ animation: `${bounce} 1s ease infinite` }}>
+        <CircularProgress />
+      </Box>
     </Box>
   );
 }
