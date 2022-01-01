e4ab1951b0832892f0e4f374cdc8f5aa651e4ff8
Rune Rasmussen <rune@ords.io>
2022-01-01 13:19:22 +0100

    adding bootstrap back

--- packages/main-application/src/index.tsx
+++ packages/main-application/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./layout/App";
-// import "./bootstrap";
+import "./bootstrap";
 
 ReactDOM.render(
   <React.StrictMode>
