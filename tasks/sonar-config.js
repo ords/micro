791587dc740a07de3bcd612a1bc4240206d8ba87
Rune Rasmussen <rune@ords.io>
2022-02-19 15:20:42 +1300

    done all changes

--- tasks/sonar-config.js
+++ tasks/sonar-config.js
@@ -1,5 +1,11 @@
 const fs = require("fs");
 const path = require("path");
+const YAML = require('yaml');
+
+const filename = "sonar-project.properties"
+const repositoryName = "Github Micro"
+const sonarOrganisation = "ords"
+const githubBuildFilePath = path.join(".github", "workflows", "sonar.yml")
 
 const nodeProjects = fs.readdirSync("packages").map((dir) => {
   try {
@@ -10,10 +16,6 @@ const nodeProjects = fs.readdirSync("packages").map((dir) => {
     return null
   }
 }).filter((value) => value !== null)
-
-const filename = "sonar-project.properties"
-const repositoryName = "Github Micro"
-const sonarOrganisation = "ords"
 
 function createConfigContent(packageName) {
 
@@ -30,6 +32,30 @@ function createFile(nodeProject) {
   fs.writeFileSync(path.join(nodeProject[0], filename), createConfigContent(nodeProject[1]))
 }
 
+const file = fs.readFileSync(githubBuildFilePath, 'utf8')
+const buildContent = YAML.parse(file)
+buildContent.jobs = {}
+
 for (let nodeProject of nodeProjects) {
-  createFile(nodeProject)
+  // createFile(nodeProject)
+  const jobID = nodeProject[1].replace("@", "").replace("/", "_")
+  buildContent.jobs[jobID] = (
+    {
+      name: 'SonarCloud',
+      'runs-on': 'ubuntu-latest',
+      steps: [
+        { uses: 'actions/checkout@v2', with: { "fetch-depth": 0 } },
+        {
+          name: 'SonarCloud Scan',
+          uses: 'SonarSource/sonarcloud-github-action@master',
+          with: { projectBaseDir: nodeProject[0] },
+          env: {
+            GITHUB_TOKEN: '${{ secrets.GITHUB_TOKEN }}',
+            SONAR_TOKEN: '${{ secrets.SONAR_TOKEN }}'
+          }
+        }
+      ]
+    })
 }
+
+fs.writeFileSync(githubBuildFilePath, YAML.stringify(buildContent))
